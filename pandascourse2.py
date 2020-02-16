62372395e9a3f49b660ca6f3132beb52a6efbb89
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-02-16 21:05:22 +0100

    drop method

--- pandascourse2.py
+++ pandascourse2.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
-movie = pd.read_csv('http://bit.ly/imdbratings')
+dataTitanic = pd.read_csv('C:/Users/wooch/Desktop/zadania/titanic.csv')
 
-print(movie.describe())
-movie.
+print( dataTitanic.shape)
+print(dataTitanic.columns)
+dataTitanic.drop( ['Surname','Name'], axis=1, inplace=True)
+
+dataTitanic.drop([0,1,2,3,4], axis=0, inplace=True)
+
+print(dataTitanic.head())
