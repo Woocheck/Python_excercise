9ec0eee4bc7ccf3b83bc47492dd2055f45cc9718
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-02-17 21:06:19 +0100

    xzcv

--- pandascourse3.py
+++ pandascourse3.py
@@ -6,3 +6,7 @@ print( dataMovie.head( 10 ) )
 print( dataMovie['title'].sort_values(ascending=False).head() )
 print( dataMovie.sort_values(['title'],ascending=False).head() )
 
+print( dataMovie[ (dataMovie['duration'] >= 200) & (dataMovie['star_rating'] >= 8.5 ) ].head())
+
+
+
--- pandascourse4.py
+++ pandascourse4.py
@@ -2,11 +2,21 @@ import pandas as pd
 import pylab as plb
 import matplotlib.pyplot as plt
 
-dataPopulation = pd.read_csv('/home/user/workspace_python/pandas/Python_excercise/WPP2019_TotalPopulationBySex.csv')
+dataPopulation = pd.read_csv('WPP2019_TotalPopulationBySex.csv')
 
-worldMidYear = dataPopulation[ dataPopulation['Location'] == 'Poland' ]
+polandMidYear = dataPopulation[ (dataPopulation['Location'] == 'Poland') & (dataPopulation['Variant'] == 'Medium') ]
+nigeriaMidYear = dataPopulation[ (dataPopulation['Location'] == 'Nigeria') & (dataPopulation['Variant'] == 'Medium') ]
+usaMidYear = dataPopulation[ (dataPopulation['Location'] == 'United States of America') & (dataPopulation['Variant'] == 'Medium') ]
+russiaMidYear = dataPopulation[ (dataPopulation['Location'] == 'Russian Federation') & (dataPopulation['Variant'] == 'Medium') ]
 
-print( worldMidYear[['Time', 'Location', 'PopTotal']] )
+plt.plot(polandMidYear['Time'],polandMidYear['PopTotal'], label='Poland')
+plt.plot(nigeriaMidYear['Time'],nigeriaMidYear['PopTotal'], label='Nigeria')
+plt.plot(usaMidYear['Time'],usaMidYear['PopTotal'], label='USA')
+plt.plot(russiaMidYear['Time'],russiaMidYear['PopTotal'], label='Russian Federation')
+
+plt.legend()
+plt.grid(True)
+plt.show()
 
 
 
