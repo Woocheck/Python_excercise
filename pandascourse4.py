800d33ba3ae1ed9c9855580bde1bcb911af3e586
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-02-18 13:50:32 +0100

    Update pandascourse4.py

--- pandascourse4.py
+++ pandascourse4.py
@@ -4,19 +4,12 @@ import matplotlib.pyplot as plt
 
 dataPopulation = pd.read_csv('WPP2019_TotalPopulationBySex.csv')
 
-polandMidYear = dataPopulation[ (dataPopulation['Location'] == 'Poland') & (dataPopulation['Variant'] == 'Medium') ]
-nigeriaMidYear = dataPopulation[ (dataPopulation['Location'] == 'Nigeria') & (dataPopulation['Variant'] == 'Medium') ]
-usaMidYear = dataPopulation[ (dataPopulation['Location'] == 'United States of America') & (dataPopulation['Variant'] == 'Medium') ]
-russiaMidYear = dataPopulation[ (dataPopulation['Location'] == 'Russian Federation') & (dataPopulation['Variant'] == 'Medium') ]
+countries =[ 'Poland', 'Nigeria', 'Germany', 'United States of America', 'Russian Federation', 'China', 'India']
 
-plt.plot(polandMidYear['Time'],polandMidYear['PopTotal'], label='Poland')
-plt.plot(nigeriaMidYear['Time'],nigeriaMidYear['PopTotal'], label='Nigeria')
-plt.plot(usaMidYear['Time'],usaMidYear['PopTotal'], label='USA')
-plt.plot(russiaMidYear['Time'],russiaMidYear['PopTotal'], label='Russian Federation')
+for country in countries:
+    MidYear = dataPopulation[ (dataPopulation['Location'] == country ) & (dataPopulation['Variant'] == 'Medium') ]
+    plt.plot(MidYear['Time'],MidYear['PopTotal'], label=country)
 
 plt.legend()
 plt.grid(True)
 plt.show()
-
-
-
