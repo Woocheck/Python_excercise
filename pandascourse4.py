bdab9c7f6c349cbc6883cff190df17152944bc9c
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-02-18 15:16:11 +0100

    g

--- pandascourse4.py
+++ pandascourse4.py
@@ -2,10 +2,10 @@ import pandas as pd
 import pylab as plb
 import matplotlib.pyplot as plt
 
-dataPopulation = pd.read_csv('WPP2019_TotalPopulationBySex.csv')
+dataPopulation = pd.read_csv('https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2019_TotalPopulationBySex.csv')
 
-countries =[ 'Poland', 'Nigeria', 'Germany', 'United States of America', 'Russian Federation', 'China', 'India']
-
+countries = [ 'Poland', 'Nigeria', 'Germany', 'United States of America', 'Russian Federation', 'China', 'India']
+variants= ['Medium', 'Low', 'High']
 for country in countries:
     MidYear = dataPopulation[ (dataPopulation['Location'] == country ) & (dataPopulation['Variant'] == 'Medium') ]
     plt.plot(MidYear['Time'],MidYear['PopTotal'], label=country)
