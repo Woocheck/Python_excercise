d5a5e588f1a157f6d51b96a828ce3e2697bcf62c
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-02-19 21:13:36 +0100

    titanic

--- pandascourse4.py
+++ pandascourse4.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pylab as plb
 import matplotlib.pyplot as plt
 
-dataPopulation = pd.read_csv('https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2019_TotalPopulationBySex.csv')
+dataPopulation = pd.read_csv('WPP2019_TotalPopulationBySex.csv')
 
 countries = [ 'Poland', 'Nigeria', 'Germany', 'United States of America', 'Russian Federation', 'China', 'India']
 variants= ['Medium', 'Low', 'High']
