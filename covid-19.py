73e3e00c4fcf0e153bd1735f028904478bf24f3a
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-03-11 22:20:39 +0100

    Create covid-19.py

---
+++ covid-19.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plotSingleCountry( country ):
+    selsctedCountries = confirmed[ confirmed['Country/Region'] == country ].transpose()
+    selsctedCountries[4:].plot()
+
+def plotConfirmed( listCountries ):
+    
+    selsctedCountries = confirmed[ confirmed['Country/Region'].isin(listCountries) ].transpose()
+    selsctedCountries.columns = selsctedCountries.iloc[1]
+    selsctedCountries[4:].plot()
+    
+    plt.title( 'Total Confirmed In Selected Countries' )
+    plt.show()
+
+
+confirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
+
+plotConfirmed(['Poland','Germany','Italy','France']) 
+
+
