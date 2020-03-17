8668cbd8929b16fd963c2cf40a8df8de4e139927
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-03-17 20:18:34 +0100

    Update avocado.py

--- avocado.py
+++ avocado.py
@@ -2,3 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 avocadoDS = pd.read_csv('https://query.data.world/s/xjgixhlxpuzocwyhznapqgynkv66gj')
+
+print( avocadoDS.head() )
+
+print( avocadoDS['Year'].unique() )
