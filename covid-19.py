c0467690d7e81b42a01373f72159c517b1218ed0
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-03-22 19:24:17 +0100

    Update covid-19.py

--- covid-19.py
+++ covid-19.py
@@ -14,6 +14,7 @@ def plotConfirmed( listCountries ):
 confirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
 
 countries = confirmed['Country/Region'].unique()
-plotConfirmed( ['Poland', 'Germany','Italy'] ) 
+
+plotConfirmed( ['Poland', 'Germany','Italy', 'Spain', 'Portugal'] )
 
 
