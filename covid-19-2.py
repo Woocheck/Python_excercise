9575f486158d6560382bbc175690330fd1ac632f
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-03-28 15:50:27 +0100

    Update covid-19-2.py

--- covid-19-2.py
+++ covid-19-2.py
@@ -25,12 +25,22 @@ def plotDeathdCases( listCountries ):
     plt.grid()
     plt.show()
 
+def plotTotalDeathdCases( listCountries ):
+    
+    newdeaths.plot(x ='date', y=listCountries, kind = 'line')
+    
+    plt.title( 'Total Deaths In Selected Countries' )
+    plt.grid()
+    plt.show()
+
 confirmed = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/total_cases.csv')
 newcases = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/new_cases.csv')
 newdeaths =  pd.read_csv('https://covid.ourworldindata.org/data/ecdc/new_deaths.csv')
-
+totaldeaths = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/total_deaths.csv')
 selectedCountries = ['Poland', 'Germany','Italy', 'Spain','United Kingdom', 'United States', 'China']
+
 plotConfirmed( selectedCountries )
 plotDeathdCases( selectedCountries )
+plotTotalDeathdCases( selectedCountries )
 plotActivecases( selectedCountries )
 
