68b56e1c6a79c905248433229c948554427fba28
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-03-31 08:51:35 +0200

    Update covid-19-2.py

--- covid-19-2.py
+++ covid-19-2.py
@@ -13,7 +13,8 @@ confirmed = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/total_cases.
 newcases = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/new_cases.csv')
 newdeaths =  pd.read_csv('https://covid.ourworldindata.org/data/ecdc/new_deaths.csv')
 totaldeaths = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/total_deaths.csv')
-selectedCountries = ['Poland', 'Germany','Italy', 'Spain','United Kingdom', 'United States', 'China']
+
+selectedCountries = ['Poland', 'Germany','Italy', 'Spain','United Kingdom', 'United States', 'China','Czech Republic']
 
 plotCases( selectedCountries, confirmed,'Total Confirmed In Selected Countries')
 plotCases( selectedCountries, newdeaths, 'Daily Confirmed Deaths In Selected Countries' )
