b29b463a2eed5be85075922d2d29760e11526c49
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-04-01 21:20:14 +0200

    Update covid-19-3.py

--- covid-19-3.py
+++ covid-19-3.py
@@ -2,24 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def plotCases( row, column, listCountries, data, diagramTitle ):
-    
-    data.plot(ax=axes[row,column], x ='date', y=listCountries, kind = 'line' )
-        
-    plt.title( diagramTitle )
-    
+    data.plot(ax=axes[row,column], x ='date', y=listCountries, kind = 'line', title = diagramTitle, grid = True, fontsize = 6, figsize = ( 8, 8.3 ) )
 
 confirmed = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/total_cases.csv')
 newcases = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/new_cases.csv')
 newdeaths =  pd.read_csv('https://covid.ourworldindata.org/data/ecdc/new_deaths.csv')
 totaldeaths = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/total_deaths.csv')
 
-selectedCountries = ['Poland', 'Germany','Italy', 'Spain','United Kingdom', 'United States', 'China','Czech Republic']
+selectedCountries = ['Poland', 'Germany', 'Sweden']
 
 fig, axes = plt.subplots(nrows=2, ncols=2)
-plotCases( 0, 0, selectedCountries, confirmed,'Total Confirmed In Selected Countries')
-plotCases( 0, 1, selectedCountries, newcases, 'Daily Confirmed Cases In Selected Countries' )
-plotCases( 1, 0, selectedCountries, newdeaths, 'Daily Confirmed Deaths In Selected Countries' )
-plotCases( 1, 1, selectedCountries, totaldeaths, 'Total Deaths In Selected Countries' )
+plotCases( 0, 0, selectedCountries, confirmed,'Total Confirmed')
+plotCases( 0, 1, selectedCountries, newcases, 'Daily Confirmed' )
+plotCases( 1, 0, selectedCountries, newdeaths, 'Daily Confirmed Deaths' )
+plotCases( 1, 1, selectedCountries, totaldeaths, 'Total Deaths' )
 
-plt.grid()
 plt.show()
