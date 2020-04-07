e652269148520df431cc62613ca0dd08dc806b8b
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-04-07 17:41:40 +0200

    Update covid-19-3.py

--- covid-19-3.py
+++ covid-19-3.py
@@ -2,14 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def plotCases( row, column, listCountries, data, diagramTitle ):
-    data.plot(ax=axes[row,column], x ='date', y=listCountries, kind = 'line', title = diagramTitle, grid = True, fontsize = 6, figsize = ( 8, 8.3 ) )
+    data.plot(ax=axes[row,column], x ='date', y=listCountries, kind = 'line', title = diagramTitle, grid = True, fontsize = 6, figsize = ( 8, 8.5 ) )
 
 confirmed = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/total_cases.csv')
 newcases = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/new_cases.csv')
 newdeaths =  pd.read_csv('https://covid.ourworldindata.org/data/ecdc/new_deaths.csv')
 totaldeaths = pd.read_csv('https://covid.ourworldindata.org/data/ecdc/total_deaths.csv')
 
-selectedCountries = ['Poland', 'Germany', 'Sweden', 'South Korea', 'Spain', 'Italy', 'Czech Republic']
+selectedCountries = ['Poland', 'Germany', 'Sweden', 'South Korea']
 
 fig, axes = plt.subplots(nrows=2, ncols=2)
 plotCases( 0, 0, selectedCountries, confirmed,'Total Confirmed')
