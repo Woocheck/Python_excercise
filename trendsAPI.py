c6f4f2d092ecac590e4a6f6c31f7323575354085
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-05-10 20:21:50 +0200

    c

--- trendsAPI.py
+++ trendsAPI.py
@@ -1,9 +1,34 @@
-import pandas as pd #pandas 0.25
+import pandas as pd
 from pytrends.request import TrendReq
+import csv
+import pandas
+
 pytrend = TrendReq()
 
-trending_searches_df = pytrend.trending_searches(pn='poland')
-print(trending_searches_df.head(40))
+pytrend.build_payload(kw_list=['covid-19'])
 
-today_searches = pytrend.today_searches(pn='US')
-print(today_searches.head(40))
+
+
+# Interest by Region
+interest_by_region_df = pytrend.interest_by_region()
+print(interest_by_region_df.head(20))
+
+# Related Queries, returns a dictionary of dataframes
+related_queries_dict = pytrend.related_queries()
+print(related_queries_dict)
+
+# Get Google Hot Trends data
+trending_searches_df = pytrend.trending_searches()
+print(trending_searches_df.head(20))
+
+# Get Google Hot Trends data
+today_searches_df = pytrend.today_searches()
+print(today_searches_df.head(20))
+
+# Get Google Top Charts
+top_charts_df = pytrend.top_charts(date = 2020, hl='en-US', tz=300, geo='PL')
+print(top_charts_df.head(20))
+
+# Get Google Keyword Suggestions
+suggestions_dict = pytrend.suggestions(keyword='poland')
+print(suggestions_dict)
