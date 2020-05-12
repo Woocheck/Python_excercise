8dd6be43779ac21b3c96b63def0b2f012a000daa
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-05-12 13:47:50 +0200

    url json from NBP

--- scrap2.py
+++ scrap2.py
@@ -2,8 +2,53 @@ import requests
 import lxml.html as lh
 import pandas as pd
 
-url='https://www.orlen.pl/PL/DlaBiznesu/HurtoweCenyPaliw/Strony/archiwum-cen.aspx?Fuel=Pb95&Year=2020'
-
+#url = "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States"
+url = "https://www.orlen.pl/PL/DlaBiznesu/HurtoweCenyPaliw/Strony/archiwum-cen.aspx?Fuel=Pb95&Year=2020"
 
 page = requests.get(url)
 
+doc = lh.fromstring(page.content)
+
+tr_elements = doc.xpath('//td')
+
+#Create empty list
+col=[]
+i=0
+#For each row, store each first element (header) and an empty list
+for t in tr_elements[0]:
+    i+=1
+    name=t.text_content()
+    #print(str(i) + ' ' + name)
+    col.append((name,[]))
+
+#Since out first row is the header, data is stored on the second row onwards
+for j in range(1,len(tr_elements)):
+    #T is our j'th row
+    T=tr_elements[j]
+    
+    #If row is not of size 10, the //tr data is not from our table 
+    if len(T)!=2:
+        break
+    
+    #i is the index of our column
+    i=0
+    
+    #Iterate through each element of the row
+    for t in T.iterchildren():
+        data=t.text_content() 
+        #Check if row is empty
+        if i>0:
+        #Convert any numerical value to integers
+            try:
+                data=int(data)
+            except:
+                pass
+        #Append the data to the empty list of the i'th column
+        col[i][1].append(data)
+        #Increment i for the next column
+        i+=1
+
+Dict={title:column for (title,column) in col}
+df=pd.DataFrame(Dict)
+
+print( df.head() )
---
+++ urljsonNBP.py
@@ -0,0 +1,25 @@
+
+import json
+import requests
+from urllib.parse import urlencode
+from urllib.parse import quote
+
+
+
+
+host = "http://api.nbp.pl/api/exchangerates/rates/a"
+currency = [ "usd", "gbp", "eur", "jpy" ]
+
+year = 2019
+beginDate = "-01-01"
+endDate = "-12-31"
+returnedFormat = { 'format' : 'json' }
+url = host +"/"+ currency[1] +"/"+ str(year) + beginDate +"/"+ str(year) + endDate +"/?"
+
+adres = url + urlencode(returnedFormat)
+
+print( adres )
+
+response = json.loads(requests.get(url).text)
+
+print(json.dumps(response, indent=4, sort_keys=True))
