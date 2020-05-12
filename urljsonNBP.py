8479d4207053c23f7b2ede0adedd1ef27df9df6b
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-05-12 14:20:31 +0200

    Update urljsonNBP.py

--- urljsonNBP.py
+++ urljsonNBP.py
@@ -7,20 +7,15 @@ import pandas as pd
 
 
 host = "http://api.nbp.pl/api/exchangerates/rates/a"
+
 currency = [ "usd", "gbp", "eur", "jpy" ]
 
 year = 2019
 beginDate = "-01-01"
 endDate = "-12-31"
-returnedFormat = { 'format' : 'json' }
 url = host +"/"+ currency[1] +"/"+ str(year) + beginDate +"/"+ str(year) + endDate +"/?"
-
-adres = url + urlencode(returnedFormat)
-
-print( adres )
 
 data = json.loads(requests.get(url).text)
 
-print(json.dumps(data, indent=4, sort_keys=True))
-
 df = pd.DataFrame.from_dict(data, orient='columns')
+print(df.head())
