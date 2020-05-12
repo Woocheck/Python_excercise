d51f2a9b096334fcc31360913bea7845fcb6ba51
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-05-12 21:26:05 +0200

    Trends working correct.

--- analyse/trendsscraping.py
+++ analyse/trendsscraping.py
@@ -5,11 +5,10 @@ import matplotlib.pyplot as plt
 
 pytrends = TrendReq()
 
-kw_list = ["Wuhan"]
-pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+kw_list = ["Wuhan", "covid", "covid-19", "Italy", "China"]
+pytrends.build_payload(kw_list, cat=0, timeframe='2019-12-01 2020-05-12', geo='', gprop='')
 
-df = pytrends.get_historical_interest(kw_list, year_start=2019, month_start=8, day_start=1, hour_start=0, year_end=2020, month_end=5, day_end=10, hour_end=0, cat=0, geo='', gprop='', sleep=0)
-
-df.plot()
+dfpl = pytrends.interest_over_time()
+dfpl.plot()
 
 plt.show()
