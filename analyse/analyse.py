add28b32d885590169111cbfc85d0f8ba560c3f5
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-05-14 17:21:25 +0200

    Update analyse.py

--- analyse/analyse.py
+++ analyse/analyse.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 def plotCases( row, column,  notowania, nazwaWaluty ):
     notowania.plot(ax=axes[row,column], x = 'effectiveDate', y = 'mid', kind = 'line', title = nazwaWaluty, grid = True, fontsize = 6, figsize = ( 8, 8.66 ) )
-currences = [ "usd", "gbp", "eur", "jpy" ]
+waluty = [ "usd", "gbp", "eur", "jpy" ]
 
 fig, axes = plt.subplots(nrows=2, ncols=2)
 x = 0
 y = 0
-for element in currences:
-   notowania = nbp.notowaniaLata( 2018, 2020, element ) 
-   plotCases( x%2, y%2,  notowania, element )
+for waluta in waluty:
+   notowania = nbp.notowaniaLata( 2019, 2020, waluta ) 
+   plotCases( x%2, y%2,  notowania, waluta )
    x+=1
    if x%2:
        y+=1
