7d7b2244e6e5de82177ec8cc9832c3939d104989
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-05-15 19:39:58 +0200

    Update orlen.py

--- analyse/orlen.py
+++ analyse/orlen.py
@@ -1,13 +1,12 @@
 import requests
 import csv
 from bs4 import BeautifulSoup as bs
+import re
 
 url = requests.get("https://www.orlen.pl/PL/DlaBiznesu/HurtoweCenyPaliw/Strony/archiwum-cen.aspx?Fuel=Pb95&Year=2020")
 soup = bs( url .content, 'html.parser')
 
-filename = 'test.csv'
+rawRecord = re.compile( r'\d\d-\d\d-\d{5} \d{3}') 
+rawData = rawRecord.findall( soup.get_text() )
 
-csv_writer = csv.writer( open( filename, 'w'))
-
-heading = soup.find('table_responsive')
-print(heading.text)
+print( rawData )
