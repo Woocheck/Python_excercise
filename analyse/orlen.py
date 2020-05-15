66c67843dc75e7ffc3d68aa91ead853bbc3b62e7
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-05-15 21:49:09 +0200

    Update orlen.py

--- analyse/orlen.py
+++ analyse/orlen.py
@@ -1,5 +1,4 @@
 import requests
-import csv
 from bs4 import BeautifulSoup as bs
 import re
 from urllib.parse import urlencode
@@ -7,20 +6,23 @@ from urllib.parse import urlencode
 def hurtoweCenyRopy( dataPoczatek, dataKoniec, rodzajPaliwa):
     listaWynik = []
     slownikGotoweDane = {}
+    #pobieranie daneych tekstowych ze stony
     for rok in range(0,dataKoniec-dataPoczatek+1):
         zwracaneDane = { 'Fuel' : rodzajPaliwa, 'Year' : dataPoczatek+rok }
         orlenUrl = "https://www.orlen.pl/PL/DlaBiznesu/HurtoweCenyPaliw/Strony/archiwum-cen.aspx?"
         urlReqest = requests.get(orlenUrl + urlencode(zwracaneDane) )
         soup = bs( urlReqest .content, 'html.parser')
-
+    #wybieranie danych tabelarycznych przy  pomocy wyrażeń regularnych
         surowyRekord = re.compile( r'\d\d-\d\d-\d\d\d\d\d\s\d\d\d') 
         suroweDane = surowyRekord.findall( soup.get_text() )
         suroweDane = [el.replace('\xa0','') for el in suroweDane]
         listaWynik.extend( suroweDane )
+    #dane tekstowe przeniesione do słownika
     for notowanie in listaWynik:
         data = notowanie[0:10]
         kwota = notowanie[10:]
         slownikGotoweDane[data] = int(kwota)
+    
     return slownikGotoweDane
     
     
