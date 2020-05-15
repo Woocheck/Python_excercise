86a8c2889a7e3fef53dcca5611b7cc1da969b102
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-05-15 21:45:56 +0200

    orlen working correct

--- analyse/orlen.py
+++ analyse/orlen.py
@@ -2,11 +2,26 @@ import requests
 import csv
 from bs4 import BeautifulSoup as bs
 import re
+from urllib.parse import urlencode
 
-url = requests.get("https://www.orlen.pl/PL/DlaBiznesu/HurtoweCenyPaliw/Strony/archiwum-cen.aspx?Fuel=Pb95&Year=2020")
-soup = bs( url .content, 'html.parser')
+def hurtoweCenyRopy( dataPoczatek, dataKoniec, rodzajPaliwa):
+    listaWynik = []
+    slownikGotoweDane = {}
+    for rok in range(0,dataKoniec-dataPoczatek+1):
+        zwracaneDane = { 'Fuel' : rodzajPaliwa, 'Year' : dataPoczatek+rok }
+        orlenUrl = "https://www.orlen.pl/PL/DlaBiznesu/HurtoweCenyPaliw/Strony/archiwum-cen.aspx?"
+        urlReqest = requests.get(orlenUrl + urlencode(zwracaneDane) )
+        soup = bs( urlReqest .content, 'html.parser')
 
-rawRecord = re.compile( r'\d\d-\d\d-\d\d\d\d\d\s\d\d\d') 
-rawData = rawRecord.findall( soup.get_text() )
-rawData = [el.replace('\xa0','') for el in rawData]
-print( rawData )
+        surowyRekord = re.compile( r'\d\d-\d\d-\d\d\d\d\d\s\d\d\d') 
+        suroweDane = surowyRekord.findall( soup.get_text() )
+        suroweDane = [el.replace('\xa0','') for el in suroweDane]
+        listaWynik.extend( suroweDane )
+    for notowanie in listaWynik:
+        data = notowanie[0:10]
+        kwota = notowanie[10:]
+        slownikGotoweDane[data] = int(kwota)
+    return slownikGotoweDane
+    
+    
+print( hurtoweCenyRopy(2018,2020,'Pb95'))
