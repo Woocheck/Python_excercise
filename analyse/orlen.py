26522a4f59b46af787bf38fcc6666c5b3cf8306b
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-05-15 20:11:48 +0200

    i have raw data from orlen

--- analyse/orlen.py
+++ analyse/orlen.py
@@ -6,7 +6,7 @@ import re
 url = requests.get("https://www.orlen.pl/PL/DlaBiznesu/HurtoweCenyPaliw/Strony/archiwum-cen.aspx?Fuel=Pb95&Year=2020")
 soup = bs( url .content, 'html.parser')
 
-rawRecord = re.compile( r'\d\d-\d\d-\d{5} \d{3}') 
+rawRecord = re.compile( r'\d\d-\d\d-\d\d\d\d\d\s\d\d\d') 
 rawData = rawRecord.findall( soup.get_text() )
-
+rawData = [el.replace('\xa0','') for el in rawData]
 print( rawData )
