521ddb06ad47a1a6b51fa78855e152d5c7a615f4
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-05-16 21:20:30 +0200

    Update trendsscraping.py

--- analyse/trendsscraping.py
+++ analyse/trendsscraping.py
@@ -2,13 +2,17 @@ import pandas as pd
 from pytrends.request import TrendReq
 import matplotlib.pyplot as plt
 
+def trendsInterestOverTime( dataPoczatek, dataKoniec, listaHasel):
+    pytrends = TrendReq()
+    przedzial = dataPoczatek+' '+dataKoniec
+    print(przedzial)
+    pytrends.build_payload(listaHasel, cat=0, timeframe= przedzial, geo='', gprop='')
+    wynik = pytrends.interest_over_time()
+    return wynik
 
-pytrends = TrendReq()
 
-kw_list = ["Wuhan", "covid", "covid-19", "Italy" , "China"]
-pytrends.build_payload(kw_list, cat=0, timeframe='2019-12-01 2020-05-12', geo='', gprop='')
 
-dfpl = pytrends.interest_over_time()
-dfpl.plot()
-
+listaHasel = ["Wuhan", "covid", "covid-19", "Italy" , "China"]
+trendy = trendsInterestOverTime( '2018-01-01', '2020-05-15',listaHasel)
+trendy.plot()
 plt.show()
