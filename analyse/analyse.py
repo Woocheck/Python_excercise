eb497a5eec89b7f737a49ea9115d414102b8edbe
Woocheck <34909593+Woocheck@users.noreply.github.com>
2020-05-19 21:48:06 +0200

    Update analyse.py

--- analyse/analyse.py
+++ analyse/analyse.py
@@ -4,7 +4,7 @@ import trendsscraping as trends
 import prezentacja as pr
 
 waluty = [ "usd", "gbp", "eur", "chf" ]
-#print(orlen.hurtoweCenyPaliw(2019,2020,'Pb95') )
-#pr.wykresCzteryWaluty( 2019, 2020, waluty)
-#print( trends.trendsInterestOverTime('2019-01-01', '2020-05-15',["Wuhan", "covid", "covid-19", "Italy" , "China"]))
+print(orlen.hurtoweCenyPaliw(2019,2020,'Pb95') )
+pr.wykresCzteryWaluty( 2019, 2020, waluty)
+print( trends.trendsInterestOverTime('2019-01-01', '2020-05-15',["Wuhan", "covid", "covid-19", "Italy" , "China"]))
 pr.wykresCzteryNaJeden(2019, 2020, waluty)
